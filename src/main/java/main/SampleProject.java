096714219adb1fb805a05c0b6687e7c814fdeb10
USER <USER@FACE-26>
2019-10-05 11:30:12 +0530

    hiii

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -4,11 +4,13 @@ public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "Hello rvabddld!";
+		String c = "Hello World!";
 		return c;
 	}
 	public static int add(int a,int b) {
-		int c = a+b;
+		int c=0;
+	 c = a+b;
+		
 		return c;
 	}
 	public static void main(String args[]) {
